f0d16b65889c1d4bba8bc8e0859883a469230484
neriomo <neriomoranp@gmail.com>
2015-05-13 21:01:37 -0430

    Pointer Basics

---
+++ chapter5/PointerBasics.cpp
@@ -0,0 +1,34 @@
+/*Nerio Moran edo Merida 13 de mayo 2015 20:41	
+Introduccion Apuntadores y Cadenas*/
+
+/*Una de las caracteristicas mas poderosas del leguaje de programacion C++ es el apuntador.
+Los apuntadores permiten simular el paso por referencia y crear y manipular estructuras 
+dinamicas de datos(es decir, que pueden crecer y dismunir), tales como las listas ligadas,
+colas, pilas y árboles.
+
+Las variables apuntador contienen direcciones de memoria como su valores. Por lo general,
+Una variable contiene de manera directa, un valor especifico.
+Un apuntador contiene la direccion de la variable que contiene un valor especifico.
+&--> representa el operador de direccion(devuelve la direccion de memoria de su operando)
+*--> representa el operador de indireccion(devuelve un sinonimo o nombre clave )
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"EJEMPLO DE PUNTEROS Y OPERADORES DE PUNTEROS\n"<<endl;
+	int *ptrEntero; //un puntero a un entero
+	int entero = 4;
+
+	ptrEntero = &entero; //a el ptrEntero se le asigna la direccion de memoria de "entero"
+
+	//ptrEntero apunta a entero.
+	cout<<"La direccion del puntero es  ----> "<<ptrEntero<<endl;
+	cout<<"El valor a el cual apunta el puntero es   ----> "<<*ptrEntero<<endl; // el * es el operador de indireccion
+
+
+
+
+	return 0;
+}
